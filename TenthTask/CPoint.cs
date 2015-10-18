ab3e9b70ce43693afaf3de94af1562208b763a61
SpoilingCode <spoilingcode@mail.ru>
2015-10-18 23:46:19 +0300

    commit

---
+++ TenthTask/CPoint.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TenthTask
+{
+    class CPoint
+    {
+        int x;
+        int y;
+        public int  X
+        {
+          get {return x; }
+          set { x = value;}
+        }
+
+        public int Y 
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        public CPoint() 
+        {
+            x = 0;
+            y = 0;
+        }
+
+        public CPoint(int x, int y) 
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+       
+    }
+}
